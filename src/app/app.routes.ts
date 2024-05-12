c781248c3580ea94b7610d2b6342ddf5ce2977aa
Max Gagner <m.gagner@hotmail.se>
2024-05-13 00:35:56 +0200

    fixes with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ConvertComponent } from './convert/convert.component';
 
 export const routes: Routes = [
-    {path: "", component: IndexComponent}
+    {path: "", component: IndexComponent},
+    {path: "gallery", component: GalleryComponent},
+    {path: "convert", component: ConvertComponent}
 ];
